76cbcdf52283ceae8c8684df9e209822dadad5eb
ValiDragan7 <valentinddragan@gmail.com>
2022-12-07 02:00:15 +0200

    dice game

---
+++ index.js
@@ -0,0 +1,19 @@
+var randomNumber1 = Math.floor(Math.random() * 6 + 1);
+var randomNumber2 = Math.floor(Math.random() * 6 + 1);
+
+var randomDiceImage = "dice" + randomNumber1 + ".png";
+var randomImageScr = "images/" + randomDiceImage;
+
+var image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src", randomImageScr);
+
+var randomImageScr2 = "images/dice" + randomNumber2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src", randomImageScr2);
+
+if (randomNumber1 > randomNumber2)
+  document.querySelector("h1").innerHTML = "Player 1 Wins!";
+else if (randomNumber1 < randomNumber2)
+  document.querySelector("h1").innerHTML = "Player 2 Wins!";
+else if (randomNumber1 === randomNumber2)
+  document.querySelector("h1").innerHTML = "Draw";
